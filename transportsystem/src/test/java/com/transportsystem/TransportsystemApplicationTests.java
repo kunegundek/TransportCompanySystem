a35f8a35d0450c093957ea387f84fdc742906451
kunegundek <tomaszpaszek1998@wp.pl>
2022-05-15 20:08:23 +0200

    First commit

---
+++ transportsystem/src/test/java/com/transportsystem/TransportsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
